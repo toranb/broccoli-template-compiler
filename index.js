71147c6f2fa12d7e64dd0d91803d80d162d3a4c7
toran billups <toranb@gmail.com>
2014-03-06 16:48:06 -0600

    more renaming for the module

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ var jsStringEscape = require('js-string-escape')
 var Filter = require('broccoli-filter')
 var compiler = require('ember-template-compiler')
 
-module.exports = TemplateFilter
-TemplateFilter.prototype = Object.create(Filter.prototype)
-TemplateFilter.prototype.constructor = TemplateFilter
-function TemplateFilter (inputTree, options) {
-  if (!(this instanceof TemplateFilter)) return new TemplateFilter(inputTree, options)
+module.exports = TemplateCompiler
+TemplateCompiler.prototype = Object.create(Filter.prototype)
+TemplateCompiler.prototype.constructor = TemplateCompiler
+function TemplateCompiler (inputTree, options) {
+  if (!(this instanceof TemplateCompiler)) return new TemplateCompiler(inputTree, options)
   this.inputTree = inputTree
   this.extensions = options.extensions
-  this.compileFunction = options.compileFunction || ''
 }
 
-TemplateFilter.prototype.targetExtension = 'js'
+TemplateCompiler.prototype.targetExtension = 'js'
 
-TemplateFilter.prototype.processString = function (string) {
+TemplateCompiler.prototype.processString = function (string) {
   var incoming = jsStringEscape(string);
   var input = compiler.precompile(incoming);
   return "Ember.TEMPLATES['application'] = Ember.Handlebars.template(" + input + ");\n";
